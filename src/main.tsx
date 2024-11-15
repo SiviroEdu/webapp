8be13b606250b4b5327cdc85648626ee1dcf2c9d
Simeon <111970903+simeonnv@users.noreply.github.com>
2024-11-15 18:33:34 +0200

    -

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
-import App from './App.tsx'
 import Home from './pages/Home.tsx';
 
 createRoot(document.getElementById('root')!).render(
