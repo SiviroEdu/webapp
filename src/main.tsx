81f83510841b10e9c8729355bedc8169b1bf3675
Simeon <111970903+simeonnv@users.noreply.github.com>
2024-11-15 18:25:56 +0200

    endpoint system (normal name)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
+import Home from './pages/Home.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<div className='bg-black'> rizz </div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home(){
+    return (
+        <div className="bg-red-600">
+            index
+            <Button>
+                RIZZZ
+            </Button>
+        </div>
+    )
+}
