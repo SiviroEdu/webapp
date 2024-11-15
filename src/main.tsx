02d1dd9baa308c95e380986e1031c2253023ed41
Simeon <111970903+simeonnv@users.noreply.github.com>
2024-11-15 23:35:37 +0200

    3d model gaming

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
 import Home from './pages/Home.tsx';
+import Nz from './pages/Nz.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
+          <Route path="Nz" element={<Nz/>} />
           <Route path="blogs" element={<div className='bg-black'> rizz </div>} />
         </Route>
       </Routes>
---
+++ src/pages/Nz.tsx
@@ -0,0 +1,32 @@
+
+import AfterLoginPage from "@/components/AfterLogInPage";
+import { Canvas, useThree } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+
+function Model() {
+    const { scene } = useGLTF("/models/base_basic_pbr.gltf");
+    const camera = useThree((state) => {
+      return state.camera;
+    });
+    //   const updateFov = (fov) => {
+    //     camera.fov = fov;
+    //     camera.updateProjectionMatrix();
+    //   };
+    return <primitive object={scene} />;
+  }
+
+export default function Nz() {
+  return (
+    <AfterLoginPage>
+        <Canvas className="h-full" camera={{ position: [0, 0, 6], fov: 25 }}>
+            <ambientLight intensity={2} />
+            <directionalLight position={[0, 3, 3]} />
+            <pointLight position={[10, 10, 10]} />
+            <mesh scale={[100, 100, 100]} />
+            <Model />
+            <OrbitControls enableZoom={true} minDistance={3} maxDistance={15} />
+        </Canvas>
+      
+    </AfterLoginPage>
+  );
+}
