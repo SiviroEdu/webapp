1c1b11fd1e1298b70b9f84a69a4374e7154a1af0
Simeon <111970903+simeonnv@users.noreply.github.com>
2024-11-16 18:46:01 +0200

    database types

---
+++ src/types/grades.ts
@@ -0,0 +1,7 @@
+export type grades = {
+    id: number,
+    created_at: Date,
+    value: string,
+    course_id: number,
+    pupil_id: number
+}
---
+++ src/types/users.ts
@@ -0,0 +1,8 @@
+export type users = {
+    id: number,
+    created_at: Date,
+    scholo_username: string,
+    coins: number | 0,
+    bulbs: number | 0,
+    pupils_id: number
+}
