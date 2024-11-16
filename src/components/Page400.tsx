525ab56775d32a714a467d3402ee5a874776fd85
Simeon <111970903+simeonnv@users.noreply.github.com>
2024-11-16 17:24:30 +0200

    neshta

---
+++ src/components/Page400.tsx
@@ -0,0 +1,22 @@
+export default function Page400(){
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-white py-48">
+            <div className="flex flex-col">
+                <div className="flex flex-col items-center">
+                    <div className="text-primary font-bold text-7xl">
+                        400
+                    </div>
+        
+                    <div className="font-bold text-black text-3xl xl:text-7xl lg:text-6xl md:text-5xl mt-10">
+                        Bad Request
+                    </div>
+        
+                    <div className="text-gray-400 font-medium text-sm md:text-xl lg:text-2xl mt-8">
+                        The server can't process or recognize the client request
+                    </div>
+                </div>
+        
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Page401.tsx
@@ -0,0 +1,21 @@
+export default function Page401(){
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-white py-48">
+            <div className="flex flex-col">
+                <div className="flex flex-col items-center">
+                    <div className="text-primary font-bold text-7xl">
+                        401
+                    </div>
+        
+                    <div className="font-bold text-black text-3xl xl:text-7xl lg:text-6xl md:text-5xl mt-10">
+                        Unauthorized
+                    </div>
+        
+                    <div className="text-gray-400 font-medium text-sm md:text-xl lg:text-2xl mt-8">
+                        The request sent to the website's server lacks valid authentication credentials.
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
