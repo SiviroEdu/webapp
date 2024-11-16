24b2e0c3844bd4cf613887cebb044f55e7dbe6da
Simeon <111970903+simeonnv@users.noreply.github.com>
2024-11-16 17:27:10 +0200

    sigma

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,16 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
 import Index from './pages/index.tsx';
-<<<<<<< Updated upstream
-=======
 import Login1 from './components/Login1.tsx';
 import Page404 from './components/page404.tsx';
->>>>>>> Stashed changes
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
       <Routes>
         <Route path="/">
           <Route index element={<Index />} />
-<<<<<<< Updated upstream
-          <Route path="blogs" element={<div className='bg-black'> rizz </div>} />
-=======
           <Route path="login" element={<Login1/>} />
           <Route path="*" element={<Page404 />} />
->>>>>>> Stashed changes
         </Route>
       </Routes>
     </BrowserRouter>
