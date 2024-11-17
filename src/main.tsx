391dc2ff92679e970ce1ca7b0c67e00715de656d
zxxtlz <hrisy2015@gmail.com>
2024-11-17 13:12:37 +0200

    Merged

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import MultiExercises from './components/MultiExercises.tsx';
 import SingleExercises from './components/SingleExercises.tsx';
 import SingleMultiplayerSelect from './components/SingleMultiplayerSelect.tsx';
 import TOS from './components/TOS.tsx';
+import Page404 from './components/page404.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
@@ -18,7 +19,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="SingleExercises" element={<SingleExercises/>} />
           <Route path="MultiExercises" element={<MultiExercises/>} />
           <Route path="SingleMultiplayerSelect" element={<SingleMultiplayerSelect/>} />
-          <Route path="*" element={<Page404 />} />
+          <Route path="*" element={<Page404/>} />
         </Route>
       </Routes>
     </BrowserRouter>
