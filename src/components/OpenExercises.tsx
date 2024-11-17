4b8502f4e79c22f5a1933491dc6189d25c122f71
zxxtlz <hrisy2015@gmail.com>
2024-11-17 10:40:31 +0200

    Can it please commit all of my files

--- src/components/OpenExercises.tsx
+++ src/components/OpenExercises.tsx
@@ -5,8 +5,16 @@ import InfoSVG from '../svg/InfoSVG';
 import { AccountData } from '../Api/GetAccountData';
 import { CacheUser } from '@/Api/Cache';
 import { Info } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+  
 
-export default function ClosedExersices(){
+export default function OpenExersices(){
     return(
         <div className="flex flex-col gap-6 w-[75%] mx-auto">
                 <header>
@@ -22,25 +30,24 @@ export default function ClosedExersices(){
                 </div>
                     <hr className="border-t-2 border-primary w-3/4 mx-auto"></hr>
                 </header>
-            <h1 className="text-black text-5xl text-center px-8">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+            <h1 className="text-black text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl text-center px-8">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+            <br />
             <center>
-                <InfoSVG classes='w-12 h-12' />
+            <Accordion type="single" collapsible className="w-[50%]">
+                <AccordionItem value="item-1">
+                    <AccordionTrigger>Expand</AccordionTrigger>
+                    <AccordionContent>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur sed necessitatibus repellat temporibus quo. Neque magni dolore consequatur modi optio adipisci? Eveniet, accusantium aspernatur mollitia atque praesentium corrupti sunt provident.
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
             </center>
-            <Button variant="outline" className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl py-6 px-4 border border-primary text-primary align-middle text-center items-center justify-center text-black">
-                a. Lorem ipsum dolor
-            </Button>
-            <Button variant="outline" className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl py-6 px-4 border border-primary text-primary align-middle text-center items-center justify-center text-black">
-                b. sit amet consectetur adipisicing elit.
-            </Button>
-            <Button variant="outline" className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl py-6 px-4 border border-primary text-primary align-middle text-center items-center justify-center text-black">
-                c. Ipsum, ducimus minus.
-            </Button>
-            <Button variant="outline" className="sm:text-lg md:text-xl lg:text-2xl xl:text-3xl py-6 px-4 border border-primary text-primary align-middle text-center items-center justify-center text-black">
-                d. Aspernatur voluptas unde ut neque soluta!
-            </Button>
-            <div className="mx-auto">
-          <Button className="w-32 bg-primary m-2">Proceed</Button>
-        </div>
+
+            <center>
+                <Textarea className="w-[50%]"/>
+                <Button className="w-32 bg-primary m-2">Proceed</Button>
+            </center>
+            
         </div>
     )
 }
