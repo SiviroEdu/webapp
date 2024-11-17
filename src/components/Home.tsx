495d7a1438a2daba95005dc98fa5a45f548c7dbd
Simeon <111970903+simeonnv@users.noreply.github.com>
2024-11-17 12:28:38 +0200

    malok

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,23 +1,50 @@
+
+import { Card, CardContent, CardFooter, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import Models from "./Models"
+import { Button } from "./ui/button"
+import LightBulbSVG from "@/svg/LightBulbSVG"
+import MoneySVG from "@/svg/MoneySVG"
+
 export default function Home(){
-    return (
-        <div className="flex-col max-w-3xl">
-        <h1 className="text-center text-4xl text-black">Welcome!</h1>
-        <br/>
-        <h2 className="text-center text-4xl text-black align-middle">
-          Login using your <span className="text-oldprimary">Shkolo</span> credentials
-        </h2>
-        <br />
-        <div className="flex align-middle w-[50%] mx-auto">
-          <h3 className="text-center text-black align-middle text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
-            orem ipsum dolor sit amet ipsum dolor sit ametipsum dolor sit ametipsum dolor sit ametipsum dolor sit ametipsum dolor sit ametipsum dolor sit ametipsum dolor sit ametipsum dolor sit amet
-          </h3>
-        </div>
-        <br />
-        <div className="flex align-middle w-[50%] mx-auto">
-          <h6 className="text-black align-middle text-center">
-            By proceeding you accept our <a href="" className="text-primary">Terms of use</a>, <a href="" className="text-primary">Privacy policy</a> and <a href="" className="text-primary">Cookie policy</a>
-          </h6>
-        </div>
+  return (
+    <div>
+
+      <Carousel className="w-full max-w-xs">
+        <CarouselContent>
+            <CarouselItem>
+              <div className="p-1">
+                <Card>
+                  <CardHeader className="!h-full flex !justify-center !items-center">
+                      <CardTitle className="text-3xl text-primary">LVL 300</CardTitle>
+                  </CardHeader>
+
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">
+                      <Models classes="!h-96 !w-96"/>
+                    </span>
+                  </CardContent>
+                </Card>
+                </div>
+            </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious className="text-black text-2xl font-black"/>
+        <CarouselNext className="text-black text-2xl font-black"/>
+      </Carousel>
+          <div className="!h-full flex !justify-center !items-center">
+          <Button variant="outline" className="flex !h-full flex-row text-primary text-2xl border-2 border-primary items-center text-center justify-center">
+              <div className='flex flex-row !w-full items-center justify-center gap-2'>
+                  <MoneySVG classes='!w-10 !h-10 fill-primary' />
+                  <p className='text-base sm:text-3xl md:text-3xl lg:text-3xl xl:text-3xl'>8000</p>
+              </div>
+          </Button>
       </div>
+    </div>
     )
 }
